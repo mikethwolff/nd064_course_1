e1cd7b32b4a3a3469c9aecfd0dd912fad8bde90b
Michael Wolff <3strom@web.de>
2023-01-27 21:02:31 +0100

    Add files via upload

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,22 @@
 import sqlite3
-
+import logging
+import os
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
-from werkzeug.exceptions import abort
+from datetime import date, datetime
+
+count = 0
+db_file= 'database.db'
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
+    try:
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        global count
+        count+=1
+    except:
+        app.logger.info("Error - Missing {}".format(db_file))
     return connection
 
 # Function to get a post using its ID
@@ -35,17 +44,46 @@ def index():
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
+    day = date.today() 
+    time = datetime.now()
     if post is None:
+      app.logger.error(day.strftime("%d/%m/%Y")+", "+time.strftime("%H:%M:%S")+" From @app.route('/<int:post_id>'): Error! Cannot retrieve Article ID: "+str(post_id)+" ") 
       return render_template('404.html'), 404
     else:
+      day = date.today() 
+      time = datetime.now()
+      app.logger.info(day.strftime("%d/%m/%Y")+", "+time.strftime("%H:%M:%S")+" From @app.route('/<int:post_id>'): Retrieved Article "+post[2]+" ")
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    time = datetime.now()
+    day = date.today()    
+    app.logger.info(day.strftime("%d/%m/%Y")+", "+time.strftime("%H:%M:%S")+" From @app.route('/about'): Retrieved About Us ")
     return render_template('about.html')
 
-# Define the post creation functionality 
+@app.route('/healthz')
+def healthcheck():
+    if os.path.exists(db_file):
+        return jsonify({"result": "OK - healthy"})
+    else:
+        return jsonify({"result": "Error - Missing {}".format(db_file)}), 500
+
+@app.route('/metrics')
+def metrics():
+    con = get_db_connection()
+    posts_count = con.execute('SELECT * FROM posts').fetchall()
+    con.close()
+    response = app.response_class(
+            response=json.dumps({"status":"success","db_connection_count": count,"post_count": str(len(posts_count))}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -60,11 +98,26 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            time = datetime.now()
+            day = date.today()  
+            app.logger.info(day.strftime("%d/%m/%Y")+", "+time.strftime("%H:%M:%S")+" From @app.route('/create'): New article created: "+title+" ")
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
-# start the application on port 3111
+def initialize_logger():
+    log_level = os.getenv("LOGLEVEL", "DEBUG").upper()
+    log_level = (
+        getattr(logging, log_level)
+        if log_level in ["CRITICAL", "DEBUG", "ERROR", "INFO", "WARNING",]
+        else logging.DEBUG
+    )
+
+    logging.basicConfig(
+        format='%(levelname)s:%(name)s:%(asctime)s, %(message)s',
+                level=log_level,
+    )
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    initialize_logger()
+    app.run(host='0.0.0.0', port='3111')
